c177e7ae1fcfe676a936fe1f8198fbef00d2339b
shahd <s12114152@stu.najah.edu>
2026-04-04 19:55:11 +0300

    Add task 4 updates

--- lib/bloc/vehicle/vehicle_bloc.dart
+++ lib/bloc/vehicle/vehicle_bloc.dart
@@ -1,105 +1,3 @@
-// import 'package:flutter_bloc/flutter_bloc.dart';
-
-// import '../../repository/storage_service.dart';
-// import '../../repository/vehicle_repository.dart';
-// import 'vehicle_event.dart';
-// import 'vehicle_state.dart';
-
-// class VehicleBloc extends Bloc<VehicleEvent, VehicleState> {
-//   final VehicleRepository repository;
-//   final StorageService storageService;
-
-//   VehicleBloc({
-//     required this.repository,
-//     required this.storageService,
-//   }) : super(VehicleInitial()) {
-//     on<LoadVehiclesEvent>(_onLoadVehicles);
-//     on<SaveVehiclesEvent>(_onSaveVehicles);
-//     on<AddVehicleEvent>(_onAddVehicle);
-//     on<DeleteVehicleEvent>(_onDeleteVehicle);
-//     on<InsertVehicleAtEvent>(_onInsertVehicleAt);
-//     on<UpdateVehicleEvent>(_onUpdateVehicle);
-//   }
-
-//   Future<void> _onLoadVehicles(
-//     LoadVehiclesEvent event,
-//     Emitter<VehicleState> emit,
-//   ) async {
-//     try {
-//       emit(VehicleLoading());
-
-//       final data = await storageService.loadAll();
-
-//       repository.setAll(
-//         motorcycles: data['motorcycles'],
-//         cars: data['cars'],
-//         trucks: data['trucks'],
-//       );
-
-//       emit(_buildLoadedState());
-//     } catch (e) {
-//       emit(VehicleError('Failed to load vehicles: $e'));
-//     }
-//   }
-
-//   Future<void> _onSaveVehicles(
-//     SaveVehiclesEvent event,
-//     Emitter<VehicleState> emit,
-//   ) async {
-//     try {
-//       await storageService.saveAll(
-//         motorcycles: repository.motorcycles,
-//         cars: repository.cars,
-//         trucks: repository.trucks,
-//       );
-
-//       emit(_buildLoadedState());
-//     } catch (e) {
-//       emit(VehicleError('Failed to save vehicles: $e'));
-//     }
-//   }
-
-//   void _onAddVehicle(
-//     AddVehicleEvent event,
-//     Emitter<VehicleState> emit,
-//   ) {
-//     repository.addVehicle(event.vehicle);
-//     emit(_buildLoadedState());
-//   }
-
-//   void _onDeleteVehicle(
-//     DeleteVehicleEvent event,
-//     Emitter<VehicleState> emit,
-//   ) {
-//     repository.deleteVehicle(event.id, event.type);
-//     emit(_buildLoadedState());
-//   }
-
-//   void _onInsertVehicleAt(
-//     InsertVehicleAtEvent event,
-//     Emitter<VehicleState> emit,
-//   ) {
-//     repository.insertVehicleAt(event.index, event.vehicle);
-//     emit(_buildLoadedState());
-//   }
-
-//   void _onUpdateVehicle(
-//     UpdateVehicleEvent event,
-//     Emitter<VehicleState> emit,
-//   ) {
-//     repository.updateVehicle(event.updated);
-//     emit(_buildLoadedState());
-//   }
-
-//   VehicleLoaded _buildLoadedState() {
-//     return VehicleLoaded(
-//       motorcycles: repository.motorcycles,
-//       cars: repository.cars,
-//       trucks: repository.trucks,
-//     );
-//   }
-// }
-//---------------------------------one
 import 'dart:async';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -119,6 +17,7 @@ class VehicleBloc extends Bloc<VehicleEvent, VehicleState> {
   final VehicleRepository repository;
   final VehicleApiService apiService;
   final CacheService cacheService;
+
   Timer? _pollingTimer;
 
   VehicleBloc({
@@ -128,8 +27,6 @@ class VehicleBloc extends Bloc<VehicleEvent, VehicleState> {
   }) : super(VehicleInitial()) {
     on<LoadVehiclesEvent>(_onLoadVehicles);
 
-    // مؤقتًا نخليه موجود حتى ما ينهار التطبيق
-    // لو لسا الشاشة بتبعث SaveVehiclesEvent
     on<SaveVehiclesEvent>(_onSaveVehicles);
 
     on<AddVehicleEvent>(_onAddVehicle);
@@ -155,24 +52,20 @@ class VehicleBloc extends Bloc<VehicleEvent, VehicleState> {
         trucks: vehicles.whereType<Truck>().toList(),
       );
 
-      // Cache successful response
       await cacheService.cacheVehicles(vehicles);
 
       emit(_buildLoadedState());
     } on NetworkUnavailableException {
-      // Try to load from cache
       await _loadFromCacheOrEmitError(
         emit,
         NetworkUnavailableState(),
       );
     } on TimeoutException {
-      // Try to load from cache
       await _loadFromCacheOrEmitError(
         emit,
         TimeoutState(),
       );
     } on ServerErrorException catch (e) {
-      // Try to load from cache
       await _loadFromCacheOrEmitError(
         emit,
         ServerErrorState(statusCode: e.statusCode),
@@ -233,7 +126,6 @@ class VehicleBloc extends Bloc<VehicleEvent, VehicleState> {
       await apiService.addVehicle(event.vehicle);
       repository.addVehicle(event.vehicle);
 
-      // Cache after add
       final allVehicles = [
         ...repository.motorcycles,
         ...repository.cars,
@@ -275,7 +167,6 @@ class VehicleBloc extends Bloc<VehicleEvent, VehicleState> {
       await apiService.deleteVehicle(event.id);
       repository.deleteVehicle(event.id, event.type);
 
-      // Cache after delete
       final allVehicles = [
         ...repository.motorcycles,
         ...repository.cars,
@@ -321,7 +212,6 @@ class VehicleBloc extends Bloc<VehicleEvent, VehicleState> {
       await apiService.addVehicle(event.vehicle);
       repository.insertVehicleAt(event.index, event.vehicle);
 
-      // Cache after insert
       final allVehicles = [
         ...repository.motorcycles,
         ...repository.cars,
@@ -363,7 +253,6 @@ class VehicleBloc extends Bloc<VehicleEvent, VehicleState> {
       await apiService.updateVehicle(event.updated);
       repository.updateVehicle(event.updated);
 
-      // Cache after update
       final allVehicles = [
         ...repository.motorcycles,
         ...repository.cars,
@@ -409,10 +298,8 @@ class VehicleBloc extends Bloc<VehicleEvent, VehicleState> {
   ) async {
     _pollingTimer?.cancel();
 
-    // Initial load
     add(LoadVehiclesEvent());
 
-    // Poll every 30 seconds
     _pollingTimer = Timer.periodic(
       const Duration(seconds: 30),
       (_) => add(LoadVehiclesEvent()),
@@ -433,4 +320,3 @@ class VehicleBloc extends Bloc<VehicleEvent, VehicleState> {
     return super.close();
   }
 }
-//------------------threeee
